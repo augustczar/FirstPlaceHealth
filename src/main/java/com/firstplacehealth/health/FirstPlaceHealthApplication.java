1db49038057addf1cca5a3b319eb3101bee50851
augustczar <augustczarramos@gmail.com>
2024-02-06 20:06:11 -0300

    initial project

---
+++ src/main/java/com/firstplacehealth/health/FirstPlaceHealthApplication.java
@@ -0,0 +1,13 @@
+package com.firstplacehealth.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstPlaceHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstPlaceHealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firstplacehealth/health/FirstPlaceHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstplacehealth.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstPlaceHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
